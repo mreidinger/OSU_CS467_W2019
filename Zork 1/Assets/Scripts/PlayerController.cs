4958b21acdedf8bdd5be91721e19095710af64d1
Christopher Seay <seayc@oregonstate.edu>
2019-01-29 11:45:02 -0800

    Area outside house, inside house, attic, & cellar
    Setup world areas of outside of the house, house ground floor, house attic, and the cellar.

--- Zork 1/Assets/Scripts/PlayerController.cs
+++ Zork 1/Assets/Scripts/PlayerController.cs
@@ -5,24 +5,43 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
      public float moveSpeed;
-     
+     public Rigidbody2D theRB;
+
+     public Animator myAnim;
+
+     public static PlayerController instance;
+
+     public string areaTransitionName;
+
      // Start is called before the first frame update
     void Start()
     {
-        
+         if(instance == null)
+         { 
+               instance = this;
+         }
+         else
+         {
+              Destroy(gameObject);
+         }
+
+         DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+         myAnim.SetFloat("moveX", theRB.velocity.x);
+         myAnim.SetFloat("moveY", theRB.velocity.y);
+
+         if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || 
+             Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
          {
-              transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+               myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+               myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
          }
 
-         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-         {
-              transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-         }
-     }
+    }
 }
