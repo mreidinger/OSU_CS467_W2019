644c2debbf49f6f8ec1273a179f766da4f015f43
Joshua <herrjosh@oregonstate.edu>
2019-01-30 20:08:34 -0800

    Initial inventory UI and scripting.

---
+++ Zork 1/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    [SerializeField]
+    private Sprite icon; //all children of this superclass must have an icon
+
+    [SerializeField]
+    private int stacksize; //if items can be stacked
+
+    public Sprite Icon
+    {
+        get => icon;
+    }
+    public int Stacksize
+    {
+        get => stacksize;
+    }
+}
