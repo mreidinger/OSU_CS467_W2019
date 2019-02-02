5543c914d9f4ee94324a03dfd6b8fd1d30798b64
mreidinger <reidingm@oregonstate.edu>
2019-02-01 19:08:15 -0500

    Handler for UI Canvas

---
+++ Zork 1/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialogueManager : MonoBehaviour
+{
+
+
+	public Text dialogText;
+	public GameObject dialogBox;
+
+	public string[] lines;
+
+	public int currentLine ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		dialogText.text = lines[currentLine];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dialogBox.activeInHierarchy)
+		{
+			if (Input.GetButtonUp("Submit"))
+			{
+				currentLine++;
+
+				if (currentLine >= lines.Length)
+				{
+					dialogBox.SetActive(false);
+				}
+				else
+				{
+					dialogText.text = lines[currentLine];
+				}
+			}
+		}
+    }
+}
